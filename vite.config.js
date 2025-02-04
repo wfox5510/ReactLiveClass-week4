2a93601ec983701e1fa3c934c137764e24e129e3
wfox5510 <wfox5510@gmail.com>
2025-02-04 15:04:28 +0800

    adjust gh-pages config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ReactLiveClass-week3/",
+  base: "/ReactLiveClass-week4/",
   plugins: [react()],
 })
